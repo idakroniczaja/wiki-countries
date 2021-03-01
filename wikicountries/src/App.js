994f23a51602755cc7893b1a6b4465abd077978b
Ida <so.kida@hotmail.com>
2021-02-28 23:15:31 -0500

    api

--- wikicountries/src/App.js
+++ wikicountries/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    apiCountries: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://restcountries.eu/rest/v2/all`)
+    .then (response => {
+       console.log(response.data)
+      let apiCountries = response.data;
+      this.setState({
+        apiCountries
+      })
+    })
+    }
+
+
+  render(){
+
+    return (
+<>
+
+<ol>
+  {this.state.apiCountries.map(each=>{
+    return(
+
+    <li> {each.name}</li>
+    )
+  })}
+</ol>
+</>
+    );
+  }
 }
 
 export default App;
